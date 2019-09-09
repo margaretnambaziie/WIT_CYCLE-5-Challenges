2013a1aa4f8505a43bab2f5a28399bab35da7770
Maggie Nambaziira <47603720+margaretnambaziie@users.noreply.github.com>
2019-09-09 13:36:00 +0300

    Add files via upload
    Challenge two "Simple Facebook Signup form"

---
+++ Facebook_homepage.js
@@ -0,0 +1,134 @@
+<html>
+<head>
+<title>Simple facebook signupForm</title>
+</head>
+<body Bgcolor="indigo">
+<hr>
+<h1><strong><center> <font color="gold"></font> Signing Up to join Facebook </br> </center></h1>
+<form id="" action="mailto:nambaziira24@gmail.com" method="get">
+</hr>
+<hr>
+<tr>
+<td>First name:<input type="text"  maxlenth = "5" name ="fname" id ="fname"></td>
+</tr>
+<tr>
+<td>Last name:<input type="text"name ="fname" id ="fname"></td><p>
+</tr>
+
+<td>User name:<input type="text" name ="fname" id ="fname"></td>
+</tr>
+<tr>
+<td>password  :</td><td><input type="password"><p></td><img src="sue1.jpg" align ="right" alt="Edit photo"/>
+<td>Tel No: </td><td><input type="months"></td>
+<td>Email Address :</td><td><input type="text"><p></td>
+</tr>
+<tr>
+<td>Adress #1:</td><td><input type="text" name ="fname" id ="fname"> </td>
+</tr>
+Adress #2:<input type="text" name ="fname" id ="fname"><P>
+
+<tr>
+ <b>GENDER:</b>
+female:<input type="radio"   name ="f" id ="f" value="female">
+male:<input type="radio"   name ="f" id ="m" value="male"><p>
+</tr>
+<tr>
+<tr>
+<label> <b>LANGUAGE:</label></b> 
+ <li><input type="checkbox" name="en" value="en" /><span>English</span></li> 
+ <li><input type="checkbox" name="nonen" value="noen"/><span>Non English</span></li>
+ <li>Others: </td><td><input type="text"><p></td>
+</tr>
+
+
+
+<td><b>District  # :<input type="text" name ="fname" id ="fname"></td></b>
+<label for="co">    COUNTRY :</label>
+<select name ="co" id ="co">
+<OPTGROUP LABEL="EAST AFRICA">
+<option>Please select your ctry:</option>
+<option>Uganda</option>
+<optionKenya</option>
+<option>Tanzania</option>
+<option>Rwanda</option>
+<option>South Sudan</option>
+</OPTGROUP>
+<OPTGROUP LABEL="ASIA">
+<option>INDIA</option>
+<option>CHINA</option>
+<option>JAPAN</option>
+</OPTGROUP>
+<OPTGROUP LABEL="AMERICA">
+<option>USA</option>
+<option>MEXICO</option>
+<option>CHILE</option>
+<option>BRAZIL</option>
+<option>COLOMBIA</option>
+</OPTGROUP>
+</select>
+</p>
+<tr>
+<td><b>STATE:<input type="text" name ="fname" id ="fname"></td>
+<td>ZIP CODE:<input type="text" name ="fname"size="5" maxlength="3"  id ="fname"></td></tr><P>
+</tr>
+</b>
+
+<tr>
+<b>STATUS:</b>
+Single:<input type="radio"   name ="s" id ="s" value="Single">
+Married:<input type="radio"   name ="m" id ="m" value="married">
+In a relationship:<input type="radio"   name ="r" id ="m" value="In a Relationship">
+Divorced:<input type="radio"   name ="d" id ="m" value="Divorced"><p>
+<tr>
+<label for="tb"><tribe>
+<b>Tribe:<input type="text" name="tribe"   size="30"><p></b>
+</tr>
+<tr>
+<td align="left"><b>Favourite team:<select name ="favourite team" id ="favourite team"></b>
+<option>manchester united</option>
+<option>manchester city</option>
+<option>arsenal</option>
+<option>chelsea</option>
+<option>liverpool</option>
+</select></td>
+</tr>
+<hr>
+<FIELDSET>
+<LEGEND ALIGN="TOP"> <b>YOUR TALENT </b>(check all that apply)</legend></b>
+<input type ="checkbox" name="nw" id ="nw" value="yes">
+<label for "nw">Codding in Any Language </label><br>
+<input type ="checkbox" name="bv" id ="bv" value="yes">
+<label for "bv">FootBall</label><br>
+<input type ="checkbox" name="win" id ="win" value="yes">
+<label for "win">Singing</label><br>
+<input type ="checkbox" name="ibm" id ="ibm" value="yes">
+<label for "ibm">Dancing</label><br>
+<input type ="checkbox" name="nw" id ="nw" value="yes">
+<label for "nw">Betting</label><br>
+<input type ="checkbox" name="pcnfs" id ="pcnfs" value="yes">
+<label for "nw">Swimming</label><br>
+<hr>
+<li><label for="desc"> <b>About Yourself (Atleast 100 characters):</label></li> <li><textarea name="desc" id="desc"></textarea></li>  </b><p>
+<input type="submit"name="dload"value="submit">
+<input type="submit"name="info"value="more info">
+<input type="reset"><br>
+
+</fieldset>
+
+
+<hr>
+<font color="gold">
+<font size="5"/>
+<center>
+<footer>
+			Copyright &copy;Facebook.com<br>
+			Developer: Margaret Nambaziira. 
+		</footer>
+ 	</center>
+
+</font>
+ 
+  
+ </form> 
+</form>
+</html>
